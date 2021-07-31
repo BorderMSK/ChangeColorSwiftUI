0195b61e9374baa18f59abbe1a1bdccc67c6912e
Igor Makeev <makeevigor@mail.ru>
2021-07-31 23:24:11 +0300

    Initial Commit

---
+++ ChangeColorSwiftUI/ChangeColorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeColorSwiftUIApp.swift
+//  ChangeColorSwiftUI
+//
+//  Created by Igor Makeev on 31.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChangeColorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
